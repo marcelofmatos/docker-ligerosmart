6836cbd57e8c4a945b4ddd619505cae2c9903428
mfmatos <46325500+mfmatos@users.noreply.github.com>
2021-08-23 16:14:45 -0300

    Update 15-cache-redis-server.sh

--- base/app-init.d/15-cache-redis-server.sh
+++ base/app-init.d/15-cache-redis-server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-[[ -z "$CACHE_REDIS_SERVER" ]] && exit 0
+[[ -z "$APP_Cache_Redis_Server" ]] && exit 0
 
 otrs.Console.pl Admin::Config::Update --setting-name "Cache::Module" --value "Kernel::System::Cache::Redis" --no-deploy
-otrs.Console.pl Admin::Config::Update --setting-name "Cache::Redis###Server" --value "$CACHE_REDIS_SERVER" --no-deploy
+otrs.Console.pl Admin::Config::Update --setting-name "Cache::Redis###Server" --value "$APP_Cache_Redis_Server" --no-deploy
