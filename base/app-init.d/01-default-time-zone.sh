ba3803e8899e0c3ac071feea214270b0cb0d6340
Marcelo Matos <marcelofmatos@gmail.com>
2021-10-13 18:09:51 -0300

    set TZ value on OTRSTimeZone

--- base/app-init.d/01-default-time-zone.sh
+++ base/app-init.d/01-default-time-zone.sh
@@ -7,3 +7,11 @@ fi;
 if [ ! -z "$APP_UserDefaultTimeZone" ]; then
     otrs.Console.pl Admin::Config::Update --setting-name 'UserDefaultTimeZone' --value "$APP_UserDefaultTimeZone" --no-deploy
 fi;
+
+if [ -z "$APP_OTRSTimeZone" ] && [ ! -z "$TZ" ]; then
+    export APP_OTRSTimeZone=$TZ
+fi;
+
+if [ ! -z "$APP_UserDefaultTimeZone" ]; then
+    otrs.Console.pl Admin::Config::Update --setting-name 'OTRSTimeZone' --value "$APP_OTRSTimeZone" --no-deploy
+fi;
