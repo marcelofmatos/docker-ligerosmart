5d84429cc07bacfc794e3b15be3a8a6ce21a6515
Marcelo Matos <marcelo.matos@ligerosmart.com>
2021-10-26 15:41:50 -0300

    elasticsearch basic config

--- base/app-init.d/10-elasticsearch.sh
+++ base/app-init.d/10-elasticsearch.sh
@@ -2,7 +2,17 @@
 
 # elasticsearch integration
 # Change WebService destination
-perl $HOME/scripts/UpdateWS.pl ${APP_LigeroSmart_Node} ${APP_CustomerID}
+
+cat >/tmp/elasticsearch-config.yml <<EOF
+---
+- ${APP_LigeroSmart_Node}:9200
+EOF
+
+otrs.Console.pl Admin::Config::Update --setting-name 'LigeroSmart###Nodes' --source-path=/tmp/elasticsearch-config.yml
+
+otrs.Console.pl Admin::Config::Update --setting-name 'LigeroSmart::Index' --value "${APP_CustomerID}"
+
+
 
 # init elasticsearch mapping
 otrs.Console.pl Admin::Ligero::Elasticsearch::MappingInstall --DefaultLanguage
