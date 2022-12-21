75d675c942177af8fc1855c31b21e1ab17672fba
mfmatos <46325500+mfmatos@users.noreply.github.com>
2022-12-21 13:41:06 -0300

    Update 10-elasticsearch.sh

--- base/app-init.d/10-elasticsearch.sh
+++ base/app-init.d/10-elasticsearch.sh
@@ -8,11 +8,9 @@ cat >/tmp/elasticsearch-config.yml <<EOF
 - ${APP_LigeroSmart_Node}:9200
 EOF
 
-otrs.Console.pl Admin::Config::Update --setting-name 'LigeroSmart###Nodes' --source-path=/tmp/elasticsearch-config.yml
-
-otrs.Console.pl Admin::Config::Update --setting-name 'LigeroSmart::Index' --value "${APP_CustomerID}"
-
-
+otrs.Console.pl Admin::Config::Update --setting-name 'LigeroSmart###Nodes' --source-path=/tmp/elasticsearch-config.yml  --no-deploy
+otrs.Console.pl Admin::Config::Update --setting-name 'LigeroSmart::Index' --value "${APP_CustomerID}"  --no-deploy
+otrs.Console.pl Maint::Config::Rebuild
 
 # init elasticsearch mapping
 otrs.Console.pl Admin::Ligero::Elasticsearch::MappingInstall --DefaultLanguage
