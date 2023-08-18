b6c0dc1e9a185f7bf1bd829f47f7422283da39ea
mfmatos <46325500+mfmatos@users.noreply.github.com>
2023-08-18 20:53:24 -0300

    fix: healthcheck

--- nginx/app-healthcheck.sh
+++ nginx/app-healthcheck.sh
@@ -2,6 +2,14 @@
 #set -x
 HEARTBEAT_FILE=/tmp/healthcheck_on
 
+if [ $(pgrep httpserver.pl) ]; then
+  exit 0
+fi
+
+if [ -z $(pgrep starman) ]; then
+  exit 0
+fi
+
 if [ ! -f "$HEARTBEAT_FILE" ]; then
   touch $HEARTBEAT_FILE
   exit 0
@@ -13,7 +21,7 @@ if test -z `find "$HEARTBEAT_FILE" -mmin +1`; then # 1 minute
 fi
 
 # WEBSERVER test
-if [ "$START_WEBSERVER" == "1" ] && [ -z $(pgrep httpserver.pl) ]; then
+if [ "$START_WEBSERVER" == "1" ]; then
     touch $HEARTBEAT_FILE
     curl -m 50 -f -s http://localhost/otrs/index.pl?healthcheck -o /dev/null || exit 1
 fi;
