8260acce009ecfc0d5d7676037ac3fee0ef8fbfe
Marcelo Matos <marcelo.matos@ligerosmart.com>
2023-08-19 02:07:03 -0400

    fix: new healthcheck

---
+++ nginx-dev/app-healthcheck.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#set -x
+HEARTBEAT_FILE=/tmp/healthcheck_on
+
+if [ $(pgrep httpserver.pl) ]; then
+  exit 0
+fi
+
+if [ -z $(pgrep starman) ]; then
+  exit 0
+fi
+
+if [ ! -f "$HEARTBEAT_FILE" ]; then
+  touch $HEARTBEAT_FILE
+  exit 0
+fi
+
+if test -z `find "$HEARTBEAT_FILE" -mmin +1`; then # 1 minute
+  # heartbeat ok
+  exit 0
+fi
+
+# WEBSERVER test
+if [ "$START_WEBSERVER" == "1" ]; then
+    curl -m 50 -f -s http://localhost/otrs/index.pl?healthcheck -o /dev/null || exit 1
+    touch $HEARTBEAT_FILE
+fi;
+
+# SCHEDULER test
+#if [ "$START_SCHEDULER" == "1" ]; then 
+#    # TODO
+#fi;
+
+
+# returns ok
+exit 0
