3795f7dd9b9116adf3115cc8e814651075d740ea
Marcelo Matos <marcelo.matos@ligerosmart.com>
2023-08-19 10:01:57 -0400

    fix: healthcheck

--- apache2/app-healthcheck.sh
+++ apache2/app-healthcheck.sh
@@ -1,19 +1,38 @@
 #!/bin/bash
+#set -x
+HEARTBEAT_FILE=/tmp/healthcheck_on
 
-# disable check on restore
-if [ ! -z "$RESTORE_DIR" ]; then 
-    exit 0
+if [ "$(pgrep -f httpserver.pl)" ]; then
+  exit 0
+fi
+
+if [ -z "$(pgrep -f apache)" ]; then
+  exit 0
+fi
+
+if [ ! -f "$HEARTBEAT_FILE" ]; then
+  touch $HEARTBEAT_FILE
+  exit 0
+fi
+
+if test -z `find "$HEARTBEAT_FILE" -mmin +1`; then # 1 minute
+  # heartbeat ok
+  exit 0
+fi
+
+# WEBSERVER test
+if [ "$START_WEBSERVER" == "1" ]; then
+    curl -m 50 -f -s http://127.0.0.1/otrs/index.pl?healthcheck -o /dev/null || exit 1
+    touch $HEARTBEAT_FILE
 fi;
 
-# FRONTEND test
-if [ "$START_FRONTEND" == "1" ] && [ -z $(pgrep httpserver.pl) ]; then 
-    curl -wfs http://localhost/otrs/index.pl?healthcheck -o /dev/null || exit 1
+# SCHEDULER test
+if [ "$START_SCHEDULER" == "1" ]; then 
+  if [ -z "$(pgrep -f otrs.Daemon.pl)" ]; then
+    exit 1
+  fi
+  touch $HEARTBEAT_FILE
 fi;
-
-# BACKEND test
-#if [ "$START_BACKEND" == "1" ]; then 
-#    # TODO
-#fi;
 
 
 # returns ok
--- nginx-dev/app-healthcheck.sh
+++ nginx-dev/app-healthcheck.sh
@@ -2,11 +2,11 @@
 #set -x
 HEARTBEAT_FILE=/tmp/healthcheck_on
 
-if [ "$(pgrep httpserver.pl)" ]; then
+if [ "$(pgrep -f httpserver.pl)" ]; then
   exit 0
 fi
 
-if [ -z "$(pgrep starman)" ]; then
+if [ -z "$(pgrep -f starman)" ]; then
   exit 0
 fi
 
@@ -22,14 +22,17 @@ fi
 
 # WEBSERVER test
 if [ "$START_WEBSERVER" == "1" ]; then
-    curl -m 50 -f -s http://localhost/otrs/index.pl?healthcheck -o /dev/null || exit 1
+    curl -m 50 -f -s http://127.0.0.1/otrs/index.pl?healthcheck -o /dev/null || exit 1
     touch $HEARTBEAT_FILE
 fi;
 
 # SCHEDULER test
-#if [ "$START_SCHEDULER" == "1" ]; then 
-#    # TODO
-#fi;
+if [ "$START_SCHEDULER" == "1" ]; then 
+  if [ -z "$(pgrep -f otrs.Daemon.pl)" ]; then
+    exit 1
+  fi
+  touch $HEARTBEAT_FILE
+fi;
 
 
 # returns ok
