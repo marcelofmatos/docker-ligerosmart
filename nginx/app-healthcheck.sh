76b01b1416e62902ed74878e5c775789606d1cc2
mfmatos <46325500+mfmatos@users.noreply.github.com>
2023-08-24 09:38:26 -0400

    fix: process check and interval

--- nginx/app-healthcheck.sh
+++ nginx/app-healthcheck.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 #set -x
 HEARTBEAT_FILE=/tmp/healthcheck_on
+HEARTBEAT_INTERVAL=${HEARTBEAT_INTERVAL:-4} # minutes
 
 if [ "$(pgrep -f httpserver.pl)" ]; then
-  exit 0
-fi
-
-if [ -z "$(pgrep -f starman)" ]; then
   exit 0
 fi
 
@@ -15,7 +12,7 @@ if [ ! -f "$HEARTBEAT_FILE" ]; then
   exit 0
 fi
 
-if test -z `find "$HEARTBEAT_FILE" -mmin +1`; then # 1 minute
+if test -z `find "$HEARTBEAT_FILE" -mmin +$HEARTBEAT_INTERVAL`; then
   # heartbeat ok
   exit 0
 fi
@@ -23,7 +20,6 @@ fi
 # WEBSERVER test
 if [ "$START_WEBSERVER" == "1" ]; then
     curl -m 50 -f -s http://127.0.0.1/otrs/index.pl?healthcheck -o /dev/null || exit 1
-    touch $HEARTBEAT_FILE
 fi;
 
 # SCHEDULER test
@@ -31,8 +27,9 @@ if [ "$START_SCHEDULER" == "1" ]; then
   if [ -z "$(pgrep -f otrs.Daemon.pl)" ]; then
     exit 1
   fi
-  touch $HEARTBEAT_FILE
 fi;
+
+touch $HEARTBEAT_FILE
 
 
 # returns ok
--- scheduler/app-healthcheck.sh
+++ scheduler/app-healthcheck.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 #set -x
 HEARTBEAT_FILE=/tmp/healthcheck_on
+HEARTBEAT_INTERVAL=${HEARTBEAT_INTERVAL:-4} # minutes
 
 if [ ! -f "$HEARTBEAT_FILE" ]; then
   touch $HEARTBEAT_FILE
   exit 0
 fi
 
-if test -z `find "$HEARTBEAT_FILE" -mmin +1`; then # 1 minute
+if test -z `find "$HEARTBEAT_FILE" -mmin +$HEARTBEAT_INTERVAL`; then
   # heartbeat ok
   exit 0
 fi
@@ -17,8 +18,9 @@ if [ "$START_SCHEDULER" == "1" ]; then
   if [ -z "$(pgrep -f otrs.Daemon.pl)" ]; then
     exit 1
   fi
-  touch $HEARTBEAT_FILE
 fi;
+
+touch $HEARTBEAT_FILE
 
 
 # returns ok
