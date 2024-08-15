7c500555db0696af281be515294c36b5a77d1c67
Marcelo Matos <marcelofmatos@gmail.com>
2024-08-14 21:40:09 -0400

    feat: database types

--- zabbix-agent/healthcheck.sh
+++ zabbix-agent/healthcheck.sh
@@ -3,6 +3,10 @@
 # exit on error
 set -o errexit
 
-ping -c 1 $MYSQL_HOST > /dev/null
-
-mysql -BN -h $MYSQL_HOST -u $MYSQL_USER -p$MYSQL_PASSWORD -e '/* ping */  SELECT 1' $MYSQL_DATABASE > /dev/null
+if [ -n "$MYSQL_HOST" ]; then
+    ping -c 1 $MYSQL_HOST > /dev/null
+    mysql -BN -h $MYSQL_HOST -u $MYSQL_USER -p$MYSQL_PASSWORD -e '/* ping */  SELECT 1' $MYSQL_DATABASE > /dev/null
+else
+    ping -c 1 $POSTGRES_DB > /dev/null
+    PGPASSWORD=$POSTGRES_PASSWORD psql -h $POSTGRES_HOST -U $POSTGRES_USER -d $POSTGRES_DB -c 'SELECT 1' > /dev/null
+fi
